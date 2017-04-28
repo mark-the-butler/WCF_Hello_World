43d6f86f75adfc3310bf5815cbc323f54b890e4b
mdb513 <mark.d.butler@sherwin.com>
2017-04-28 13:53:51 -0400

    Added an IHelloWorldService that the concrete HelloWorldService can implement

---
+++ HelloWorld/HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldService
+{
+    public class HelloWorldService : IHelloWorldService
+    {
+        public String GetMessage(string name)
+        {
+            return $"Hello world from {name}!";
+        }
+    }
+}
--- HelloWorld/HelloWorldService/IHelloWorldService.cs
+++ HelloWorld/HelloWorldService/IHelloWorldService.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceModel;
 
 namespace HelloWorldService
 {
-    interface IHelloWorldService
+    [ServiceContract]
+    public interface IHelloWorldService
     {
+        [OperationContract]
+        string GetMessage(string name);
     }
 }
